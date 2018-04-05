4cd6287ec9b6fdfb4641673df702c64e55cf25f5
hendrikblog <38091746+hendrikblog@users.noreply.github.com>
2018-04-05 15:37:55 +0800

    Membuat Class Aksi dan Method main

---
+++ src/praktek01/MahasiswaAksi.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package praktek01;
+
+/**
+ *
+ * @author LENOVO 320
+ */
+public class MahasiswaAksi {
+    
+}
