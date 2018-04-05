bbb433d9acc91be730231a56185076fc192afac7
hendrikblog <38091746+hendrikblog@users.noreply.github.com>
2018-04-05 15:53:59 +0800

    Mengisi nilai atribut

--- src/praktek01/MahasiswaAksi.java
+++ src/praktek01/MahasiswaAksi.java
@@ -12,6 +12,10 @@ package praktek01;
 public class MahasiswaAksi {
     public static void main(String[] args) {
         Mahasiswa Harry = new Mahasiswa();
+        
+        Harry.nama="Harry Potter";
+        Harry.NPM="19639999";
+        Harry.IPK=3.4;
     }
     
 }
