88d6b1e4a442ce9645e95e52fb5a61b2f5cd433b
hendrikblog <38091746+hendrikblog@users.noreply.github.com>
2018-04-05 14:54:51 +0800

    Initial commit

---
+++ src/praktek01/Praktek01.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package praktek01;
+
+/**
+ *
+ * @author LENOVO 320
+ */
+public class Praktek01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
